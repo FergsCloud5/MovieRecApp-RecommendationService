887026512a3957974fc04ea8203275dbcdaa6f96
Daktor99 <ed2918@columbia.edu>
2021-10-11 00:55:30 -0400

    Updated RDBService to handle requests

--- database_services/RDBService.py
+++ database_services/RDBService.py
@@ -1,45 +1,117 @@
 import pymysql
 import json
+import logging
 import middleware.context as context
 
-
-def _get_db_connection():
-
-    db_connect_info = context.get_db_info()
-
-    print("Connection info = \n", json.dumps(db_connect_info, indent=2, default=str))
-
-    db_connection = pymysql.connect(**db_connect_info)
-    return db_connection
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-def get_by_prefix(db_schema, table_name, column_name, value_prefix):
+class RDBService:
 
-    conn = _get_db_connection()
-    cur = conn.cursor()
+    def __init__(self):
+        pass
 
-    sql = "select * from " + db_schema + "." + table_name + " where " + \
-        column_name + " like " + "'" + value_prefix + "%'"
-    print("SQL Statement = " + cur.mogrify(sql, None))
+    @classmethod
+    def _get_db_connection(cls):
 
-    res = cur.execute(sql)
-    res = cur.fetchall()
+        db_connect_info = context.get_db_info()
 
-    conn.close()
+        logger.info("RDBService._get_db_connection:")
+        logger.info("\t HOST = " + db_connect_info['host'])
 
-    return res
+        db_info = context.get_db_info()
 
-def get_full_resource(db_schema, table_name):
+        db_connection = pymysql.connect(
+            **db_info,
+            autocommit=True
+        )
+        return db_connection
 
-    conn = _get_db_connection()
-    cur = conn.cursor()
+    @classmethod
+    def get_full_resource(cls, db_schema, table_name):
 
-    sql = "select * from " + db_schema + "." + table_name
-    print("SQL Statement = " + cur.mogrify(sql, None))
+        conn = RDBService._get_db_connection()
+        cur = conn.cursor()
 
-    res = cur.execute(sql)
-    res = cur.fetchall()
+        sql = "select * from " + db_schema + "." + table_name
+        print("SQL Statement = " + cur.mogrify(sql, None))
 
-    conn.close()
+        res = cur.execute(sql)
+        res = cur.fetchall()
 
-    return res
+        conn.close()
+        return res
+
+    @classmethod
+    def run_sql(cls, sql_statement, args, fetch=False):
+
+        conn = RDBService._get_db_connection()
+
+        try:
+            cur = conn.cursor()
+            res = cur.execute(sql_statement, args=args)
+            if fetch:
+                res = cur.fetchall()
+        except Exception as e:
+            conn.close()
+            raise e
+
+        return res
+
+    @classmethod
+    def get_where_clause_args(cls, template):
+
+        terms = []
+        args = []
+        clause = None
+
+        if template is None or template == {}:
+            clause = ""
+            args = None
+        else:
+            for k, v in template.items():
+                terms.append(k + "=%s")
+                args.append(v)
+
+            clause = " where " + " AND ".join(terms)
+
+        return clause, args
+
+    @classmethod
+    def find_by_template(cls, db_schema, table_name, template):
+
+        wc, args = RDBService.get_where_clause_args(template)
+
+        conn = RDBService._get_db_connection()
+        cur = conn.cursor()
+
+        sql = "select * from " + db_schema + "." + table_name + " " + wc
+        res = cur.execute(sql, args=args)
+        res = cur.fetchall()
+
+        conn.close()
+
+        return res
+
+    @classmethod
+    def create(cls, db_schema, table_name, create_data):
+
+        cols = []
+        vals = []
+        args = []
+
+        for k, v in create_data.items():
+            cols.append(k)
+            vals.append('%s')
+            args.append(v)
+
+        cols_clause = "(" + ",".join(cols) + ")"
+        vals_clause = "values (" + ",".join(vals) + ")"
+
+        sql_stmt = "insert into " + db_schema + "." + table_name + " " + cols_clause + \
+                   " " + vals_clause
+
+        res = RDBService.run_sql(sql_stmt, args)
+        return res
